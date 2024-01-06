b1135650ca02491bc4b89658604f6c591dac477d
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-06 18:10:27 +0000

    strings updated

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -11,3 +11,12 @@ console.log(str.charAt(0));
 console.log(str[7]);
 console.log(str.concat(" America"));
 console.log(str.toUpperCase());
+console.log(str.substring(2,7));
+console.log(str.substring(7));
+
+console.log(str.slice(2,5)); //It will return
+console.log(str.slice(-13, 7)); //It will return
+console.log(str.replace("Sta", "Mta")); //It will replace
+console.log(str.includes("ted")); //It will return boolean
+
+console.log(str.split(" ")); //It will make it array
