80275926ec30ae6bab38a603d884c21b6b55bbec
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-06 13:08:25 +0000

    comparison and equality check

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,23 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);   // == is equality operator & <, >, <=, >= is comparison operator.
+console.log(null >= 0);  //Here "ONLY" comparison converts "null" value into "0" hence output is TRUE
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+// === is Strict check value and datatype also.
+
+console.log("2" === 2);
