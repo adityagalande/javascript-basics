b220b5392bf3693c295d9e62f91c4aaf3d3fd1f2
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-06 09:40:47 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+let playerName = "Rohit";
+let mostOdiRuns = 264;
+let avg = 45.57;
+let isRightHand = true;
+let retirementDate;
+let penalties = null;
+
+console.table([typeof playerName, typeof mostOdiRuns, typeof avg, typeof isRightHand, typeof retirementDate, typeof penalties]);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log("typeof undefined : " + typeof undefined)
+console.log("typeof null : " + typeof null)
