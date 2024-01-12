92c537d9aa3048eac832aa1cccde72449c8fb1bf
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-12 18:20:37 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,8 +6,8 @@ Shallow copy : means have same referance points (change in source Obj reflects i
 Deep copy : means "do not have" same referance points (change in source Obj Not reflects in copy Obj)
 */
 
-// Declaration of Array
 
+// Declaration of Array
 let arr1 = [1,2,3, "Yes", true];
 let arr2 = new Array(1,2,3);
 
@@ -20,8 +20,10 @@ console.log(arr1.indexOf(3)); //Gives index value in array.
 let giveStringArray = arr2.join(); //This converts array into string
 console.table([typeof giveStringArray, giveStringArray]);
 
+
 let concatArray = arr1.concat(arr2)
 console.log(concatArray.length);
+
 // ---------------------------------------------------
 let arr3 = [1,2,3,4,5]
 
@@ -36,4 +38,18 @@ console.log(arr3);
 
 arr3.shift() // 9 is removed from front end
 console.log(arr3);
-// ---------------------------------------------------
+
+// -----------------------SCLICE----------------------------
+// Working of slice and splice
+
+let arr4 = [1,2,3,4,5]
+
+let arr5 = arr4.slice(1,3); //It prints elements at index 1 & 2 and exclude index 3. Also it dont make change in original array.
+console.log(`sliced array -> ${arr5}`) 
+console.log(`original array -> ${arr4}`)
+// -------------------------SPLICE--------------------------
+let arr6 = [9,8,7,6,5]
+
+let arr7 = arr6.splice(1,3); //It prints elements at index 1, 2 and 3. Also it make change in original array. By excluding element, whose index is kept in splice method.
+console.log(`spliced array -> ${arr7}`) 
+console.log(`original array -> ${arr6}`)
