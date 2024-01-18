2a2b8b108809c7348aa204a7cfeb07e34c90e796
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-18 17:47:34 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,7 @@
 //Using constructor object is singleton [ Object.create ]
 
+const mySymbol = Symbol("USSR");
+
 //Object Literals
 const JsUser = {
     name: "Vladimir",
@@ -8,10 +10,28 @@ const JsUser = {
     email: "vladimir.lenin@URLSearchParams.com",
     isLoggedIn: true,
     lastLoginDays: ["Mon", "Tue", "Wed", "Sat"],
-    "capital city": "Moscow"
+    "capital city": "Moscow",
+    [mySymbol]: "Union Of Soviet Socialist Republic" //This is how we put Symbol datatype key in Object
 };
 
 console.table([JsUser.name, JsUser["email"]]);  //Here you have to put age in "" coz in JsUser age key stored as string -> "age"
 
+console.log(JsUser["capital city"]); //If you have space in key then you have to use [] notation insted of . notation
 
-console.log(JsUser["capital city"]); //If you have space in key then you have to use [] insted of .
+console.log(JsUser[mySymbol]); //This is how we call Symbol datatype in Object Key.
+
+//We can put function in Object
+JsUser.language = function(){
+    console.log("Slavic Russian");
+};
+
+console.log(JsUser.language());
+
+
+JsUser.age = 28; //value is modified.
+
+Object.freeze(JsUser); //Now we can not change any value entire JsUser Obj.
+
+JsUser.age = 78;
+console.log(JsUser["age"]);
+console.log(JsUser)
