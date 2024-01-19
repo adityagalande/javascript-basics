55db2ea171030ac00befcbddbd062f9743ca9ced
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-19 10:35:31 +0000

    Object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,5 +6,11 @@ const Obj = new Object();  //It is singleton Obj
 Obj.id = `12345abc`;
 Obj.name = "Joe";
 Obj.isLoggedIn = true;
+Obj["full name"] = {
+    firstName: "Joseph",
+    middleName: "Robinette",
+    lastName: "Biden"
+};
 
+console.log(Obj["full name"].lastName);
 console.log(Obj);
