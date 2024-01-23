1d1c4dfdf1bae65ad3dfe8fe810878424ef80344
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-23 09:48:29 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,8 +10,13 @@ myName(); //This is execution of function [Parenthesis after function definition
 
 console.log(typeof myName)
 
+//These are [parameters] while defining function
 function addNums(num1, num2){
     return num1 + num2;
 }
 
+// These are [arguments] while calling function
 console.log(addNums(9,9));
+
+const res = addNums(7,5);
+console.log(`Result : ${res}`);
