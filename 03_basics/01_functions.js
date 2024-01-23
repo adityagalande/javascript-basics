02ff55c54e2ba75874ab747b15cf808a4f88a353
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-23 08:01:21 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,12 @@
+
+
+
+
+function myName(){
+    console.table([`Aditya`, `Ulhas`, `Galande`])
+}
+
+myName; //This is function referance
+myName(); //This is execution of function [Parenthesis after function definition means it is execution]
+
+console.log(typeof myName)
