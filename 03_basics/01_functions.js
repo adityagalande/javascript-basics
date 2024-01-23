8a0d7e2445d096efb4c6ad3631cc32703a6a5a0b
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-01-23 09:35:29 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,4 @@
-
-
+//Function
 
 
 function myName(){
@@ -10,3 +9,9 @@ myName; //This is function referance
 myName(); //This is execution of function [Parenthesis after function definition means it is execution]
 
 console.log(typeof myName)
+
+function addNums(num1, num2){
+    return num1 + num2;
+}
+
+console.log(addNums(9,9));
