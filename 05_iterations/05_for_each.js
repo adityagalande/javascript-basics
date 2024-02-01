7fda864897fc83fd1e43a3fc40c591f3004e42e6
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-02-01 13:44:48 +0000

    for each loop

---
+++ 05_iterations/05_for_each.js
@@ -0,0 +1,42 @@
+const arr = ["CRPF", "BSF", "CISF", "ITBP", "SSB", "NDRF", "SRPF"];
+
+arr.forEach(element => {
+    console.log(element)
+});
+
+
+// ------------------------------------
+// Another way to use For Each loop
+
+function printMe(item){
+    console.log(item);
+}
+// Here give referance of that function
+arr.forEach(printMe);
+
+
+// ------------------------------------
+// Here it has value, index of item & Array list
+arr.forEach( (value, index, arrList) => {
+    console.log(value, index, arrList)
+})
+
+
+// ------------------------------------
+// Here Objects inside Array
+
+const objArr = [
+    {
+        name: "JavaScript",
+        fileName: "js"
+    },
+    {
+        name: "Java",
+        fileName: "java"
+    },
+    {
+        name: "Python",
+        fileName: "py"
+    }
+];
+
