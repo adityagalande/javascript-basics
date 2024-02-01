ec7aa1feb4642deeb38faf2b41b3096be7401d1d
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-02-01 13:44:29 +0000

    for of loop

--- 05_iterations/03_for_of_loop.js
+++ 05_iterations/03_for_of_loop.js
@@ -2,19 +2,41 @@ let arr = ["IND", "USA", "CAD", "PAK", "UAE"];
 
 // For of loop
 for (const iterator of arr) {
-    console.log(iterator)
+    // console.log(iterator)
 }
 
 const str = "Hello JavaScript!!";
 // Here string char by char printed
 for(const iterator of str){
-    console.log(iterator)
+    // console.log(iterator)
 }
 
 
-// map is Obj, which holdes key value pairs. It keep insertion order of key
+// map is Obj, which holdes unique key value pairs. It keep insertion order of key
 const map = new Map();
+map.set('IND', "India")
+map.set('USA', `United States of America`)
+map.set('BA', `Bosnia And Herzegovina`)
+map.set('FR', `France`)
+map.set('KG', `KYRGYZSTAN`)
 
+for(const [key, val] of map){
+    // Here val gives array like -: [IND, India]
+    // console.log(val)
+
+    // Here use Array de-structuring
+    console.log(`Code ${key} : ${val}`);
+}
+
+// console.log(map)
+// console.log(map.get('BA'))
+
+
+// -------------------------------------------------------
+// Here Array de-structuring.
+let a,b;
+({a, b} = {a: 2, b: 3})
+console.log(`${a}   ${b}`);
 
 
 const obj = {
@@ -22,5 +44,14 @@ const obj = {
     p: 2,
     m: 3
 }
-
 console.log(Object.values(obj));
+console.log(Object.keys(obj));
+
+
+
+
+// If you use For in loop on Array it will give only keys not values
+for (const key in arr) {
+    console.log(key);
+    // console.log(arr[key]);
+}
