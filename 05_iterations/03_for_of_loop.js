83f6c57970d8babdd72abce47f097b9290a965b6
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-02-01 07:33:20 +0000

    for of loop

---
+++ 05_iterations/03_for_of_loop.js
@@ -0,0 +1,12 @@
+let arr = ["IND", "USA", "CAD", "PAK", "UAE"];
+
+// For of loop
+for (const iterator of arr) {
+    console.log(iterator)
+}
+
+const str = "Hello JavaScript!!";
+// Here string char by char printed
+for(const iterator of str){
+    console.log(iterator)
+}
